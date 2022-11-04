9501af46d084a6a5afb87f6598eb6eee58861833
Esad Dursun <3723esad@gmail.com>
2022-11-04 22:57:50 +0100

    course-4 exercise db_dynamodb_create exports handler created

--- course-4/exercise/db_dynamodb_create/index.js
+++ course-4/exercise/db_dynamodb_create/index.js
@@ -8,6 +8,29 @@ const groupsTable = process.env.GROUPS_TABLE;
 
 exports.handler = async (event) => {
   console.log("Processing event: ", event);
+  const itemId = uuid.v4();
 
-  return {};
+  const parsedBody = JSON.parse(event.body);
+
+  const newItem = {
+    id: itemId,
+    ...parsedBody,
+  };
+
+  await docClient
+    .put({
+      TableName: groupsTable,
+      Item: newItem,
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      newItem,
+    }),
+  };
 };
